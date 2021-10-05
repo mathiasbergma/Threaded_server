88d811e3bd1e5e5714147242544db488d0081a53
Mathias <mathiasbergma@gmail.com>
2021-10-05 20:10:54 +0200

    Mathias

---
+++ Socket.c
@@ -0,0 +1,16 @@
+/*
+ * Socket.c
+ *
+ *  Created on: Oct 5, 2021
+ *      Author: mathias
+ *
+ *      Threaded Server
+ */
+
+#include <stdio.h>
+#include <pthread.h>
+
+int main(int ArgC, char **ArgV)
+{
+
+}
